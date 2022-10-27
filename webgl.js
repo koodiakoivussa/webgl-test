7119aa3161c3a5982c1ee0c2c4d62a83414737c7
Koivu <koodiakoivussa@gmail.com>
2022-10-27 12:09:19 +0300

    reformat of vertexShaderText and fragmentShaderTest

--- webgl.js
+++ webgl.js
@@ -4,31 +4,32 @@ https://www.youtube.com/watch?v=kB0ZVUrI4Aw
 */
 
 const vertexShaderText = 
-[    
-    'precision mediump float;',
-    '',
-    'attribute vec2 vertPosition;',
-    'attribute vec3 vertColor;',
-    'varying vec3 fragColor;',
-    '',
-    'void main()',
-    '{',
-    '   fragColor = vertColor;',
-    '   gl_Position = vec4(vertPosition, 0.0, 1.0);',
-    '}'
-].join('\n');
+`
+    precision mediump float;
+    
+    attribute vec2 vertPosition;
+    attribute vec3 vertColor;
+    varying vec3 fragColor;
+    
+    void main()
+    {
+       fragColor = vertColor;
+       gl_Position = vec4(vertPosition, 0.0, 1.0);
+    }
+`;
 
 const fragmentShaderText = 
-[
-    'precision mediump float;',
-    '',
-    'varying vec3 fragColor;',
-    'uniform float uTime;',
-    'void main()',
-    '{',
-    '   gl_FragColor = vec4(fragColor, 1.0) + (0.25 + 0.25 * vec4(cos(uTime + fragColor.xyx + vec3(0,3,6)), 0)) ;',
-    '}'
-].join('\n');
+`
+    precision mediump float;
+    
+    varying vec3 fragColor;
+    uniform float uTime;
+    
+    void main()
+    {
+       gl_FragColor = vec4(fragColor, 1.0) + (0.25 + 0.25 * vec4(cos(uTime + fragColor.xyx + vec3(0,3,6)), 0));
+    }
+`;
 
 var InitDemo = function (){
 
