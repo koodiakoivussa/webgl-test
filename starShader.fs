d4854b8c375e24a1eb61dd2a07c9e22bdf652cb7
Koivu <koodiakoivussa@gmail.com>
2022-10-30 15:57:16 +0200

    updated starShader.fs

--- starShader.fs
+++ starShader.fs
@@ -4,6 +4,15 @@
     uniform float uTime;
     uniform vec2 uResolution;
 
+    /*
+    Shader test based on The Art of Codes(youtube) tutorial
+    https://www.youtube.com/watch?v=rvDo9LvfoVE
+    */
+
+    mat2 Rotate(float a){
+        float s=sin(a), c=cos(a);
+        return mat2(c, -s, s, c);
+    }
 
     float r21(vec2 p){
         p = fract(p*vec2(123.34, 456.21));
@@ -14,23 +23,16 @@
     float createOrb(vec2 uv, float glow){
         float distanceToCenter = length(uv);
         float orb = glow/distanceToCenter;
-        orb *= smoothstep(1.0, 0.0, distanceToCenter);
+        orb *= smoothstep(1., 0., distanceToCenter);
         return orb;
     }
 
-    void main()
-    {
-        // Normalize resolution and coordinates
-        vec2 res = uResolution / uResolution;
-        vec2 uv = fract((fragColor.xy*res.xy) / res.y);
+    vec3 createOrbfield(vec2 uv, float scale){
 
-        float scale = 20.;
+        vec3 orbfield = vec3(0.);
 
         vec2 grid = 0.5-fract(uv * scale);
-        vec2 id = floor(uv * scale);
-        vec3 o = vec3(0.);
-
-        
+        vec2 id = floor(uv * scale);       
 
         for(int y=-1; y <= 1; y++){
 
@@ -40,14 +42,47 @@
                 float n = r21(id+offset);
                 float size = fract(n*345.25);
 
-                float orb = createOrb( grid + offset - vec2(n, fract(n*12.)) + .5, .03);
+                float orb = createOrb( grid + offset - vec2(n, fract(n*12.)) + .5, .05);
                 vec3 color = sin(vec3(.2, .3, .9) * fract(n*2345.45)*123.3)*.5+.5;
-                color = color*vec3(1, 0, 1);
+                color = color*vec3(1., 1.0, 0.+size);
 
-                o += orb * size * color;
+                orb *= sin(uTime*3.+n*6.2831)*.5+1.;
+
+                orbfield += orb * size * color;
                 
             }
         }
 
+        return orbfield;
+    }
+
+    void main()
+    {
+        // Normalize resolution and coordinates
+        vec2 res = uResolution / uResolution;        
+        vec2 uv = .5-fract((fragColor.xy*res.xy) / res.y);    
+
+        // This mess somehow holds the aspect ratio together and I have no idea why
+        float dimy = uResolution.y/uResolution.x;
+        float dimx = uResolution.x/uResolution.y;
+        if (dimx > dimy){
+            uv.y *= dimy;
+        }
+        if (dimx < dimy) {
+            uv.x *= dimx;
+        }
+        
+        float time = uTime * 0.1;
+        uv *= Rotate(time);
+        vec3 o = vec3(0.);
+        float layers = 5.;
+        
+        for(float i = 0.; i < 1.; i += 1./8.){
+            float depth = fract(i+time);
+            float fade = depth*smoothstep(1., .9, depth);
+            float scale = mix(20., .5, depth);
+            o += createOrbfield(uv * scale + i * 432.12, 2.)*fade;
+        }
+        
         gl_FragColor = vec4(o, 1.);
     }
